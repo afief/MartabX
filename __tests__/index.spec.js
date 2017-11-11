fa0b8d3a81d98fb65d52c89e1fbc82e745cb2b85
Afief Yona Ramadhana <ramacyber@gmail.com>
2017-11-11 17:23:15 +0700

    :rocket: update tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -4,54 +4,103 @@ const Entry = require('./models/Entry')
 const faker = require('faker')
 
 describe('Basic CRUD', () => {
-  let currentUser = null
 
-  it('Create : Success', async () => {
-    const name = faker.name.findName()
-    const user = await User.create({
-      name
+  describe('Success Story', () => {
+    let currentUser = null
+
+    it('Create : Success', async () => {
+      const name = faker.name.findName()
+      const user = await User.create({
+        name
+      })
+
+      expect(!isNaN(user.id)).toBe(true)
+      expect(user.name).toBe(name)
+      expect(user.created_at).toBeTruthy()
+
+      currentUser = user
     })
 
-    expect(!isNaN(user.id)).toBe(true)
-    expect(user.name).toBe(name)
-    expect(user.created_at).toBeTruthy()
+    it('Read : Success', async () => {
+      const user = await User.find(currentUser.id)
 
-    currentUser = user
+      expect(!isNaN(user.id)).toBe(true)
+      expect(user.name).toBe(currentUser.name)
+    })
+
+    it('Update : Success', async () => {
+      const currentName = currentUser.name
+      const newName = faker.name.findName()
+
+      const user = await User.find(currentUser.id)
+      user.name = newName
+      const result = await user.update()
+
+      expect(result).toBe(true)
+      expect(user.name).toBe(newName)
+
+      currentUser = user
+    })
+
+    it('Delete : Success', async () => {
+      const currentName = currentUser.name
+      const newName = faker.name.findName()
+
+      const user = await User.find(currentUser.id)
+      user.name = newName
+      const result = await user.delete()
+
+      expect(result).toBe(true)
+
+      const oldUser = await User.find(currentUser.id)
+      expect(oldUser).toBe(null)
+    })
   })
 
-  it('Read : Success', async () => {
-    const user = await User.find(currentUser.id)
+  describe('Failed Story', () => {
 
-    expect(!isNaN(user.id)).toBe(true)
-    expect(user.name).toBe(currentUser.name)
-  })
+    it('Failed to create row due to empty column', async () => {
+      const user2 = await User.create()
+      expect(user2).toBe(null)
+    })
 
-  it('Update : Success', async () => {
-    const currentName = currentUser.name
-    const newName = faker.name.findName()
+    it('Failed to create row due to invalid column', async () => {
+      const user = await User.create({
+        nama: faker.name.findName(),
+      })
+      expect(user).toBe(null)
+    })
 
-    const user = await User.find(currentUser.id)
-    user.name = newName
-    const result = await user.update()
+    it('Failed to get row due to id not found', async() => {
+      const user = await User.find(faker.random.number(10000, 20000))
+      expect(user).toBe(null)
+    })
 
-    expect(result).toBe(true)
-    expect(user.name).toBe(newName)
+    it('Failed to get row due to id not a number', async() => {
+      const user = await User.find(faker.address.city())
+      expect(user).toBe(null)
+    })
 
-    currentUser = user
-  })
+    it('Failed to update row due to invalid parameter', async() => {
+      const user = await User.create({
+        name: faker.name.findName()
+      })
+      user.address = faker.address.streetName()
 
-  it('Delete : Success', async () => {
-    const currentName = currentUser.name
-    const newName = faker.name.findName()
+      let result = null
+      try {
+        result = await user.update()
+      } catch (e) {
+        expect(e.code).toBe('SQLITE_ERROR')
+      }
+      expect(result).toBe(null)
+    })
 
-    const user = await User.find(currentUser.id)
-    user.name = newName
-    const result = await user.delete()
+    it('Failed to delete row due to id not found', async() => {
+      const result = await User.delete(faker.random.number(10000, 20000))
+      expect(result).toBe(false)
+    })
 
-    expect(result).toBe(true)
-
-    const oldUser = await User.find(currentUser.id)
-    expect(oldUser).toBe(null)
   })
 
 })
